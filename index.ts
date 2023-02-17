f9781fcc361aca2f266fcc5d6a170b240e338a93
rutexd <rutexgreat@gmail.com>
2023-02-17 01:28:07 +0100

    Moved config out of src

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import config from "./src/config";
+import config from "./config";
 import {  GatewayIntentBits } from "discord.js";
 import color from "colors/safe";
 import DiscordFileStorageApp, { printAndExit } from "./src/DiscordFileStorageApp";
