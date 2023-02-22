7489fcb5dc0fe61290fbbf12c7c6c80c44f2ec1b
rutexd <rutexgreat@gmail.com>
2023-02-22 05:23:47 +0100

    Env checker fix

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
-import {  GatewayIntentBits } from "discord.js";
+dotenv.config();
+
+import { GatewayIntentBits } from "discord.js";
 import color from "colors/safe";
 import DiscordFileStorageApp, { printAndExit } from "./src/DiscordFileStorageApp";
 import WebdavFilesystemHandler from "./src/webdav/WebdavFilesystemHandler";
@@ -7,20 +9,21 @@ import { v2 as webdav } from "webdav-server";
 import WebdavServer from "./src/webdav/WebdavServer";
 import fs from "node:fs";
 import root from "app-root-path";
- 
+
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0 as any;
 //Without it throws error: cause: Error [ERR_TLS_CERT_ALTNAME_INVALID]: Hostname/IP does not match certificate's altnames: Host: localhost. is not in the cert's altnames: DNS: ***
 // Idk how to fix it now, so let it be just disabled.
 
 async function main() {
-    dotenv.config();
     const token = checkEnvVariableIsset("TOKEN", "Please set the TOKEN to your bot token.");
     const guildId = checkEnvVariableIsset("GUILD_ID", "Please set the GUILD_ID to your guild id.");
-    const startWebdavServer = checkEnvVariableIsset<boolean>("START_WEBDAV", "Please set the START_WEBDAV to true or false to start webdav server.");
-    const webdavPort = checkEnvVariableIsset<number>("PORT", "Please set the PORT to your webdav server port.");
     const filesChannelName = checkEnvVariableIsset("FILES_CHANNEL", "Please set the FILES_CHANNEL to your files channel name.");
     const metaChannelName = checkEnvVariableIsset("META_CHANNEL", "Please set the META_CHANNEL to your meta channel name.");
-    const enableHttps = checkEnvVariableIsset<boolean>("ENABLE_HTTPS", "Please set the ENABLE_HTTPS to true or false to enable https.");
+
+    const webdavPort = checkEnvVariableIsset("PORT", "Please set the PORT to your webdav server port.") as number;
+
+    const startWebdavServer = checkEnvVariableIsset("START_WEBDAV", "Please set the START_WEBDAV to true or false to start webdav server.") as boolean;
+    const enableHttps = checkEnvVariableIsset("ENABLE_HTTPS", "Please set the ENABLE_HTTPS to true or false to enable https.", "boolean") as boolean;
 
     const app = new DiscordFileStorageApp({
         intents: [
@@ -42,9 +45,9 @@ async function main() {
             rootFileSystem: new WebdavFilesystemHandler(app),
         }
 
-        if(enableHttps){
+        if (enableHttps) {
             console.log("Detected ENABLE_HTTPS env variable. Starting webdav server with https enabled.");
-            
+
             // generate self-signed certificate: openssl req -x509 -newkey rsa:4096 -keyout privkey.pem -out cert.pem -days 365 -nodes
             checkIfFileExists(root.resolve("/certs/privkey.pem"), false, "Please set ssl ./certs/privKey.pem or generate a self-signed certificate");
             checkIfFileExists(root.resolve("/certs/cert.pem"), false, "Please set ssl ./certs/cert.pem or generate a self-signed certificate");
@@ -55,57 +58,78 @@ async function main() {
                 cert: readFileSyncOrUndefined(root.resolve("/certs/cert.pem")),
                 ca: readFileSyncOrUndefined(root.resolve("/certs/chain.pem")),
             }
-        }else{
-            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0 as any; // in case if we started the server without https on machine with any bounded domain name and https cert, enabled cert verification will fail in some cases. so we disable this check.
         }
-        
+
         const webdavServer = new WebdavServer(serverLaunchOptions);
 
-        await webdavServer.startAsync(webdavPort);
+        await webdavServer.startAsync();
         console.log(color.green("WebDAV server started at port " + webdavPort + "."));
 
-        webdavServer.beforeRequest((arg, next) => {
-            let pathInfo = arg.requested.path;
-            if(pathInfo.paths[0] === "get"){
-                console.log("wtf");
-            }
+        // debug
+        // webdavServer.beforeRequest((arg, next) => {
+        //     let pathInfo = arg.requested.path;
+        //     if(pathInfo.paths[0] === "get"){
+        //         console.log("wtf");
+        //     }
 
-            next();
-        });
+        //     next();
+        // });
 
-        webdavServer.afterRequest((arg, next) => {
-            // Display the method, the URI, the returned status code and the returned message
-            // console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
-            // If available, display the body of the response
-            // console.log(arg.responseBody);
-            next();
-        });
+        // webdavServer.afterRequest((arg, next) => {
+        // Display the method, the URI, the returned status code and the returned message
+        // console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+        // If available, display the body of the response
+        // console.log(arg.responseBody);
+        // next();
+        // });
     }
 
 }
 
-function checkIfFileExists(path: string, soft: boolean, assertString: string = ""): boolean  {
-    try{
-        if(!fs.statSync(path).isFile()){
-            const string = "File "+ path +" is not found" + (assertString.length > 0 ? ": " + assertString : "" );
-            if(!soft){
+function checkIfFileExists(path: string, soft: boolean, assertString: string = ""): boolean {
+    try {
+        if (!fs.statSync(path).isFile()) {
+            const string = "File " + path + " is not found" + (assertString.length > 0 ? ": " + assertString : "");
+            if (!soft) {
                 throw new Error(string);
             }
             console.warn(string);
             return false;
         }
-    }catch(e){
+    } catch (e) {
         return false;
     }
     return true;
 }
 
-function checkEnvVariableIsset<T = string>(name: string, assertString: string): T {
-    const value = process.env[name];
-    if(!value){
-        printAndExit("Env variable "+ name +" is not set" + (assertString.length > 0 ? ": " + assertString : "" ) + ". Please set it in .env file or in your system environment variables.");
+function checkEnvVariableIsset(name: string, assertString: string, type: "string" | "number" | "boolean" = "string"): any {
+    const value = process.env[name]!;
+    if (!value) {
+        printAndExit("Env variable " + name + " is not set" + (assertString.length > 0 ? ": " + assertString : "") + ". Please set it in .env file or in your system environment variables.");
     };
-    return value as unknown as T;
+
+    const valueLower = value.toLowerCase();
+
+    if (type == "boolean") {
+        if (valueLower === "true") {
+            return true;
+        } else if (valueLower === "false") {
+            return false;
+        } else {
+            printAndExit("Env variable " + name + " is not set to true or false" + (assertString.length > 0 ? ": " + assertString : "") + ". Please set it in .env file or in your system environment variables.");
+        }
+    }
+
+    if (type == "number") {
+        const number = parseInt(value!);
+        if (isNaN(number)) {
+            printAndExit("Env variable " + name + " is not set to number" + (assertString.length > 0 ? ": " + assertString : "") + ". Please set it in .env file or in your system environment variables.");
+        }
+        return number;
+    }
+
+    return value;
+
 }
 
 function readFileSyncOrUndefined(path: string): string | undefined {
@@ -124,6 +148,7 @@ process.on("uncaughtException", (err) => {
     console.log(color.red(err.name));
     console.log(color.red(err.message));
     console.log(color.red(err.stack!));
+    printAndExit("Uncaught exception, to prevent data loss, the app will be closed.");
 });
 
 main();
