474d896c6aed205e189d0af0ef529d35590ff149
rutexd <rutexgreat@gmail.com>
2023-04-06 19:15:37 +0200

    Moved from testyts to mocha

---
+++ tests/main.test.ts
@@ -0,0 +1,9 @@
+import { createClient } from "webdav"
+import { assert } from "chai";
+import { describe, it } from "mocha";
+
+describe("Initial mocha test", () => {
+	it("Initial mocha test", async () => {
+		assert.equal(5, 5);
+	});
+});
